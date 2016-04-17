9841c09fdb664aa8120f92e386651ce6d8c5eac3
K-YO-HI <pythagora.yoshimoto@gmail.com>
2016-04-17 20:27:24 +0900

    System.h説明追加

--- Scripts/SukenLib/System/System.h
+++ Scripts/SukenLib/System/System.h
@@ -8,148 +8,148 @@ namespace suken{
 class CSystem{
 public:
 /**
-*	@brief 説明
+*	@brief コンストラクタ　メンバ変数などの初期化を行う
 */
 	CSystem();
 /**
-*	@brief 説明
+*	@brief デストラクタ　DxLibの後片付け等
 */
 	~CSystem();
 /**
-*	@brief 説明
+*	@brief DxLibの初期化、初期設定などを担当
 */
 	void Awake();
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief exeが立ち上がってからの時間をマイクロ秒で返す
+* @return exeが立ち上がってからの時間[マイクロ秒]
 */
 	int  GetLocalStandardTime();
 /**
-*	@brief 説明
-*	@param value 引数
+*	@brief void Awake()をバックグラウンドで実行するかどうかを設定
+*	@param flag true:void Awake()をバックグラウンドで実行する false:通常通り（初期値）
 */
 	void SetUseThread_Awake(bool flag);
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief void Awake()をバックグラウンドで実行するかどうかを取得
+* @return true:void Awake()をバックグラウンドで実行する false:通常通り（初期値）
 */
 	bool GetUseThread_Awake();
 /**
-*	@brief 説明
-*	@param value 引数
+*	@brief ローディング時に表示する画像を指定する
+*	@param path 画像のパス 
 */
 	void SetLoadingGraph(const std::string path);
 /**
-*	@brief 説明
-*	@param value 引数
+*	@brief ローディングの最低時間をマイクロ秒で設定
+*	@param time ローディングの最低時間[マイクロ秒] 
 */
 	void SetLoadingMinimalTime(int time);
 /**
-*	@brief 説明
+*	@brief システム用関数
 */
 	void Wait_Loading();
 /**
-*	@brief 説明
+*	@brief システム用関数
 */
 	void Update();
 /**
-*	@brief 説明
+*	@システム用関数
 */
 	void Wait();
 /**
-*	@brief 説明
+*	@brief システム用関数
 */
 	void End();
 /**
-*	@brief 説明
+*	@brief スクリーンショットが取れるはずの関数（未整備）
 */
 	void TakeScreenShot();
 /**
-*	@brief 説明
-*	@param value 引数
+*	@brief スクリーンショットが取れるはずの関数（未整備）
+*	@param  GrHandleAdress　スクリーンショットの画像ハンドルを格納先を示すint型ポインタ
 */
 	void GetScreenShot(int *GrHandleAdress);
 /**
-*	@brief 説明
+*	@brief この関数を実行することでexeが終了する
 */
 	void Escape();
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief exeを終了するかどうかを得る（システム関数）
+* @return true:exeが次フレームで終了する　false:exe実行中
 */
 	bool GetEscapeFlag();
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief exeが立ち上がってからのフレーム数を取得
+* @return exeが立ち上がってからのフレーム数
 */
 	int GetFrame();
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief 現フレームの始まった時間をマイクロ秒で取得（フレーム内で時間を統一したいときにオススメ）
+* @return 現フレームの始まった時間[マイクロ秒]
 */
 	int GetNow();
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief 現在のフレームレートを取得（60フレームに１回更新）
+* @return フレームレート
 */
 	float GetFps();
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief 使用中のディスプレイの更新が１秒間に何回行われるかを取得
+* @return ディスプレイのリフレッシュレート
 */
 	int GetRefreshRate();
 /**
-*	@brief 説明
-*	@param value 引数
+*	@brief 理想となるフレームレートを指定する（処理が重い場合や、指定したフレームレートがディスプレイの更新頻度より高い場合は指定したフレームレートより遅くなります）
+*	@param _fps　理想のフレームレート
 */
 	void SetTargetFps(float _fps);
 /**
-*	@brief 説明
+*	@brief 理想となるフレームレートを取得する
 *	@param value 引数
 * @return 戻り値
 */
 	float GetTargetFps();
 /**
-*	@brief 説明 //新しいスレッドを作る
-*	@param value 引数
+*	@brief 説明 //新しいスレッドを作る（exe終了時に正常に解放されます）
+*	@param pFunc 作成したスレッドで実行される関数のポインタ 
 */
 	void CreateNewThread( void(*pFunc)() );
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief ディスプレイのデバイスコンテクストを取得（システム関数）
+* @return ディスプレイのデバイスコンテクスト
 */
 	HDC GetDisplayDC();
 #ifdef USE_LUA
 /**
-*	@brief 説明
-* @return 戻り値
+*	@brief 未整備
+* @return 
 */
 	lua_State* GetLua();
 #endif
 private:
-	HDC display ;///<  @brief 説明//ディスプレイドライバ
-	int frame;///<  @brief 説明
-	int count;///<  @brief 説明
-	int startTime;///<  @brief 説明
-	int localStandardTime;///<  @brief 説明
-	int N;		///<  @brief 説明	//Nフレームに一回fpsを算出する
-	float fps;///<  @brief 説明
-	float targetFps;///<  @brief 説明
-	int now;///<  @brief 説明
-	int refreshRate;///<  @brief 説明
-	std::vector<HANDLE> handleChild;///<  @brief 説明
-	bool escapeFlag;///<  @brief 説明
-	int screenShot;///<  @brief 説明
-	bool screenShotFlag;///<  @brief 説明
-	int *screenShotGrHandleAdress;///<  @brief 説明
-	std::string loadingImgPath;///<  @brief 説明
-	int loadingImg;///<  @brief 説明
-	bool useThread_AwakeFlag;///<  @brief 説明
-	int loadingMinimalTime;///<  @brief 説明
+	HDC display ;///<  @brief ディスプレイデバイスコンテクストを格納
+	int frame;///<  @brief exe開始時からのフレーム数を格納
+	int count;///<  @brief フレームレート算出用変数
+	int startTime;///<  @brief フレームレート算出用変数
+	int localStandardTime;///<  @brief exe開始時からの時間をマイクロ秒で格納
+	int N;		///<  @brief フレームレート算出用変数 (Nフレームに一回fpsを算出する)
+	float fps;///<  @brief フレームレートを格納
+	float targetFps;///<  @brief 理想とするフレームレートを格納（初期値：６０）
+	int now;///<  @brief 現フレームが始まった時間を格納
+	int refreshRate;///<  @brief 使用中のディスプレイの１秒間の更新回数を格納
+	std::vector<HANDLE> handleChild;///<  @brief 作成したスレッドのハンドルを格納
+	bool escapeFlag;///<  @brief exeを終了するかどうかのフラグ
+	int screenShot;///<  @brief スクリーンショットの画像ハンドルを格納するための入れ物
+	bool screenShotFlag;///<  @brief 現フレームのスクリーンショットをとるかどうか
+	int *screenShotGrHandleAdress;///<  @brief スクリーンショットの画像ハンドルを格納先の示すint型ポインタを格納
+	std::string loadingImgPath;///<  @brief Loading画面に表示する画像のパスを格納
+	int loadingImg;///<  @brief Loading画面に表示する画像ハンドルを格納
+	bool useThread_AwakeFlag;///<  @brief void Awake()をバックグラウンドで実行するかどうかのフラグ
+	int loadingMinimalTime;///<  @brief Loadingに費やす最低時間を格納（マイクロ秒）
 #ifdef USE_LUA
 	lua_State *Lua;///<  @brief 説明
 #endif
 };
 }
 //Singleton
-extern suken::CSystem System;///<  @brief 説明
+extern suken::CSystem System;///<  @brief 
